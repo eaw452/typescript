aed88eb50af48a65bbc13a6f6c76bb405dc98eac
eaw452 <erik.weissenborn@gmail.com>
2022-08-21 16:55:01 -0400

    added type predicates and discriminated unions

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
 "use strict";
-var Empty = /** @class */ (function () {
-    function Empty() {
-    }
-    return Empty;
-}());
+class Empty {
+}
 function check(valid) {
     console.log(valid.name);
 }
-var j = { x: 5, y: 9, name: "me" };
+const j = { x: 5, y: 9, name: "me" };
 check(j);
 function greet(person, date) {
-    console.log("Hello ".concat(person, ", today is ").concat(date.toDateString(), "!"));
+    console.log(`Hello ${person}, today is ${date.toDateString()}!`);
 }
 greet("Barry", new Date());
 function greetOptional(obj) {
@@ -37,7 +34,7 @@ function printID(id) {
 }
 printID("twenty");
 printID(20);
-var greeting = "hello";
+let greeting = "hello";
 console.log(greeting);
 //literal types - must be one of the strings listed or of Options type (object with greeting property)
 function printGreeting(greet) {
@@ -60,8 +57,7 @@ console.log(dosomething2(3));
 //perform truthiness check on strs to ensure it is not null  "strs && etc."
 function printAll(strs) {
     if (strs && typeof strs === "object") {
-        for (var _i = 0, strs_1 = strs; _i < strs_1.length; _i++) {
-            var s = strs_1[_i];
+        for (const s of strs) {
             console.log(s); //could be null!!  will not compile !
         }
     }
@@ -93,7 +89,84 @@ function move(animal) {
 function example() {
     console.log("fly example!");
 }
-var bird1 = { fly: function () { console.log("fly!"); } };
+var bird1 = { fly: () => { console.log("fly!"); } }; //use of anonymous func
 var bird2 = { fly: example };
 move(bird1);
 move(bird2);
+//use of instanceof  - checks whether obj's prototype chain contains Date
+function isDate(obj) {
+    if (obj instanceof Date) {
+        console.log(obj.toDateString());
+    }
+    else {
+        console.log(obj);
+    }
+}
+isDate(new Date());
+isDate("the date");
+//asignments  - TypeScript will evaluate right side of '=' sign, and narrow down the variable type
+let a = 5 ? "good" : 5; //a is given type number | string by TS
+a = 10; //a can be assigned a number OR a string
+a = "good"; //a can be assigned a number OR a string
+let b = "good"; //b is given type string by TS
+//b = 10;  //cannot assign a number to b, as b is type string
+b = "bye";
+//Control flow analysis of TS
+function padLeft(padding, input) {
+    if (typeof padding === "number") {
+        return " ".repeat(padding) + input;
+    }
+    //return "".repeat(padding) + input;  //will not compile becuase TS knows this must be a string
+    return padding + input;
+}
+console.log(padLeft(10, "hello padded on left"));
+//although x is declared with union type string|number|boolean , the function
+//can only return x as a string or number due to flow of execution 
+function xTypes() {
+    let x;
+    x = Math.random() < 0.5;
+    console.log(x); //x is a boolean here
+    if (Math.random() < 0.5) {
+        x = 100;
+    }
+    else {
+        x = "string type";
+    }
+    return x;
+}
+console.log(xTypes());
+//Using Type Predicates
+function getSmallPet() {
+    const x = Math.random();
+    if (x < 0.5) {
+        return { swim: () => { console.log("swimming"); } };
+    }
+    return { fly: () => { console.log("flying"); } };
+}
+//Type predicate is used as a return type for this function  'paramName is type'
+function isFish(pet) {
+    //return (pet as Fish).swim !== null; //results in true regardless - maybe with target es2015
+    return Boolean(pet.swim);
+}
+let pet = getSmallPet();
+//Type predicates allows us to instead of using the below type assertions 'varName as type'
+if (pet.swim) {
+    pet.swim(); //this is messy
+}
+else {
+    pet.fly(); //this is messy
+}
+//You can use the function with the predicate for custom type checking
+if (isFish(pet)) {
+    pet.swim(); //TS knows that pet is a Fish here
+}
+else {
+    pet.fly(); //TS knows that pet must NOT be a Fish here, so it is therefore a Bird
+}
+function getArea(shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * Math.pow(shape.radius, 2); //now TS knows shape is Circle
+    }
+    return Math.pow(shape.sideLength, 2); //here, shape must be a Square because shape.kind !== "circle"
+}
+console.log(getArea({ kind: "circle", radius: 5 }));
--- index.ts
+++ index.ts
@@ -139,6 +139,108 @@ var bird2: Bird = { fly: example };
 move(bird1);
 move(bird2);
 
+//use of instanceof  - checks whether obj's prototype chain contains Date
+function isDate(obj: Date | string) {
+    if (obj instanceof Date) {
+        console.log(obj.toDateString());
+    } else {
+        console.log(obj);
+    }
+}
+isDate(new Date());
+isDate("the date");
+
+//asignments  - TypeScript will evaluate right side of '=' sign, and narrow down the variable type
+let a = 5 ? "good" : 5;  //a is given type number | string by TS
+a = 10;  //a can be assigned a number OR a string
+a = "good";  //a can be assigned a number OR a string
+
+
+let b = "good";  //b is given type string by TS
+//b = 10;  //cannot assign a number to b, as b is type string
+b = "bye";
+
+
+//Control flow analysis of TS
+function padLeft(padding: number | string, input: string) {
+    if (typeof padding === "number") {
+        return " ".repeat(padding) + input;
+    }
+    //return "".repeat(padding) + input;  //will not compile becuase TS knows this must be a string
+    return padding + input;
+}
+console.log(padLeft(10, "hello padded on left"));
+
+//although x is declared with union type string|number|boolean , the function
+//can only return x as a string or number due to flow of execution 
+function xTypes(): string | number {
+    let x: string | number | boolean;
+    x = Math.random() < 0.5;
+    console.log(x);  //x is a boolean here
+
+    if (Math.random() < 0.5) {
+        x = 100;
+    } else {
+        x = "string type";
+    }
+
+    return x;
+}
+console.log(xTypes());
+
+
+//Using Type Predicates
+function getSmallPet(): Fish | Bird {
+    const x = Math.random();
+    if (x < 0.5) {
+        return { swim: () => { console.log("swimming"); } };
+    }
+    return { fly: () => { console.log("flying"); } };
+}
+//Type predicate is used as a return type for this function  'paramName is type'
+function isFish(pet: Bird | Fish): pet is Fish {
+    //return (pet as Fish).swim !== null; //results in true regardless - maybe with target es2015
+    return Boolean((pet as Fish).swim);
+}
+
+let pet: Fish | Bird = getSmallPet();
+//Type predicates allows us to instead of using the below type assertions 'varName as type'
+if ((pet as Fish).swim) {
+    (pet as Fish).swim();  //this is messy
+} else {
+    (pet as Bird).fly();  //this is messy
+}
+//You can use the function with the predicate for custom type checking
+if (isFish(pet)) {
+    pet.swim();  //TS knows that pet is a Fish here
+} else {
+    pet.fly();  //TS knows that pet must NOT be a Fish here, so it is therefore a Bird
+}
+
+//Discriminated Unions
+//Sometimes we need to have a common property among different custom types, 
+//which can be used to discern between the types. This can be helpful when a function 
+//takes a parameter of a union type, such as (Circle | Square) or (ClientMessage | ServerMessage).
+//This common property allows TS to narrow down the potential types that a variable may be.
+interface Circle {
+    kind: "circle";
+    radius: number;
+}
+interface Square {
+    kind: "square";
+    sideLength: number;
+}
+type Shape = Circle | Square;
+
+function getArea(shape: Shape) {
+    if (shape.kind === "circle") {
+        return Math.PI * shape.radius ** 2;  //now TS knows shape is Circle
+    }
+    return shape.sideLength ** 2;  //here, shape must be a Square because shape.kind !== "circle"
+}
+
+console.log(getArea({ kind: "circle", radius: 5 }));
+
 
 
 
